2870ed5caa220279ea092bb5ef61eaf64dedf505
Mattias <mattias.astrom@hotmail.com>
2025-03-12 10:25:04 +0100

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ import Game from "./pages/Game";
 function App() {
   return (
     <>
-      <HashRouter basename="/reactportfolio">
+      <HashRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/reactportfolio" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Projects" element={<Projects />} />
           <Route path="/Cv" element={<Cv />} />
